02c50a28089629e3f7d7f789b64835314d473e3f
DityaManral11 <dityaguesswhy1102@gmail.com>
2026-01-19 10:53:03 +0530

    Updated code

--- script.js
+++ script.js
@@ -118,11 +118,13 @@
 // form.addEventListener("Submit", handlesubmit)
 
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Form Submitted!")
-}
-const form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit)
+// const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log("Form Submitted!")
+// }
+// const form = document.querySelector('form');
+// form.addEventListener('submit', handleSubmit)
+
+console.log("Updated Code!")
 
 
