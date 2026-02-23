552cc2fa300899cbc33adc02306404505089491d
DityaManral11 <dityaguesswhy1102@gmail.com>
2026-02-23 09:35:15 +0530

    update

--- script.js
+++ script.js
@@ -568,17 +568,87 @@
 // console.log("Last Line")
 
 
-async function getData(){
-    try{
-        const response = await fetch ("https://dummyjson.com/products/200")
-        console.log(response.ok)
-        if(response.ok===false)throw new Error("Data not found")
-        const data=await response.json()
-        console.log(data)
-        // data.products.forEach((ele)=>{
-        //     console.log(ele.price)
-        // })
-    }catch(error){
-        console.log("Data not found")
-    }
+// async function getData(){
+//     try{
+//         const response = await fetch ("https://dummyjson.com/products/200")
+//         console.log(response.ok)
+//         if(response.ok===false)throw new Error("Data not found")
+//         const data=await response.json()
+//         console.log(data)
+//         // data.products.forEach((ele)=>{
+//         //     console.log(ele.price)
+//         // })
+//     }catch(error){
+//         console.log("Data not found")
+//     }
+// }
+
+
+// async function sendData(){
+//     try{
+//         const response = await fetch("https://dummyjson.com/products/add", {
+//             method: "POST",
+//             headers: {"content-type":"application/json"},
+//             body: JSON.stringify({
+//                 title:"Macbook",
+//                 description:"Macbook Pro",
+//                 price:1000000,
+//                 discountPercentage:5,
+//                 rating:4.5,
+//                 stock:5,
+//                 brand:"Apple",
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//     }catch(error){
+//         console.log("Data not found!")
+//     }
+// }
+// sendData()
+
+
+// localStorage.setItem("name", "Ditya")
+// localStorage.setItem("age", 18)
+// console.log(localStorage.getItem("name"))
+// console.log(localStorage.getItem("age"))
+
+
+// let obj={
+//     title:"Macbook",
+//     description:"Macbook Pro"
+// }
+
+// localStorage.setItem("obj",JSON.stringify(obj))
+// localStorage.setItem("name", "Ditya")
+// localStorage.setItem("age", 18)
+// console.log(localStorage.getItem("name"))
+// console.log(localStorage.getItem("age"))
+// console.log(JSON.parse(localStorage.getItem("obj")))
+
+// localStorage.removeItem("age")
+
+// localStorage.clear()
+
+
+// let obj={
+//     title:"Macbook",
+//     description:"Macbook Pro"
+// }
+// sessionStorage.setItem("obj",JSON.stringify(obj))
+// sessionStorage.setItem("name", "Ditya")
+// sessionStorage.setItem("age", 18)
+// console.log(sessionStorage.getItem("name"))
+// console.log(sessionStorage.getItem("age"))
+// console.log(JSON.parse(sessionStorage.getItem("obj")))
+
+
+document.cookie="name:Ditya; expires=Sat 21 Feb 2026 12:00:00 UTC"
+document.cookie="age:18; expires=Sat 21 Feb 2026 12:00:00 UTC"
+
+console.log(document.cookie)
+
+async function sample(){
+    await fetch("https://127.0.0.1.5500/index.html")
 }
+sample()
